db25be13e3555c9c377a8b9975feb01ff6d937e8
JandersonLSilva <janderson.user@gmail.com>
2024-09-03 12:08:59 -0300

    Código inicial dos modelos de Admin e Client

--- models/Admin.js
+++ models/Admin.js
@@ -1,4 +1,71 @@
 const { DataType, Op } = require('sequelize');
 const sequelize = require('../helpers/db');
 
+// Modelo de Admin
+const AdminModel = sequelize.define('admin', {
+    cpf: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: {
+                msg: 'O CPF não deve ser vazio.'
+            },
+            len: {
+                args: [11],
+                msg: 'O CPF deve ter exatamente 11 digitos.'
+            },
+            is: {
+                args: /^[0-9]+$/,
+                msg: 'O CPF deve ter somente valores numéricos.'
+            }
+        }
+    },
+    fullName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: {
+                msg: 'O Nome não deve ser vazio.'
+            },
+            len: {
+                args: [5, 30],
+                msg: 'O Nome deve ter entre 5 e 20 caracteres.'
+            },
+            is: {
+                args: /^[a-b]+$/i,
+                msg: 'Não é permitido caracteres além de letras no Nome.'
+            }
+        }
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        isEmail: {
+            msg: 'Insira um endereço de email válido.'
+        },
+        validate: {
+            notEmpty: {
+                msg: 'O E-mail não deve ser vazio.'
+            }
+        }
+    },
+    contact_number: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: {
+                msg: 'O Número para Contato não deve ser vazio.'
+            },
+            len: {
+                args: [11],
+                msg: 'O Número para Contato deve ter exatamente 11 digitos.'
+            },
+            is: {
+                args: /^[0-9]+$/,
+                msg: 'O Número para Contato deve ter somente valores numéricos.'
+            }
+        }
+    }
+});
+
 module.exports = AdminModel;
--- models/Client.js
+++ models/Client.js
@@ -1,4 +1,87 @@
 const { DataType, Op } = require('sequelize');
 const sequelize = require('../helpers/db');
 
+const ClientModel = sequelize.define('client', {
+    cpf: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: {
+                msg: 'O CPF não deve ser vazio.'
+            },
+            len: {
+                args: [11],
+                msg: 'O CPF deve ter exatamente 11 digitos.'
+            },
+            is: {
+                args: /^[0-9]+$/,
+                msg: 'O CPF deve ter somente valores numéricos.'
+            }
+        }
+    },
+    fullName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: {
+                msg: 'O Nome não deve ser vazio.'
+            },
+            len: {
+                args: [5, 30],
+                msg: 'O Nome deve ter entre 5 e 30 caracteres.'
+            },
+            is: {
+                args: /^[a-b]+$/i,
+                msg: 'Não é permitido caracteres além de letras no Nome.'
+            }
+        }
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        isEmail: {
+            msg: 'Insira um endereço de email válido.'
+        },
+        validate: {
+            notEmpty: {
+                msg: 'O E-mail não deve ser vazio.'
+            }
+        }
+    },
+    contact_number: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: {
+                msg: 'O Número para Contato não deve ser vazio.'
+            },
+            len: {
+                args: [11],
+                msg: 'O Número para Contato deve ter exatamente 11 digitos.'
+            },
+            is: {
+                args: /^[0-9]+$/,
+                msg: 'O Número para Contato deve ter somente valores numéricos.'
+            }
+        }
+    },
+    address: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: {
+                msg: 'O Endereço não deve ser vazio.'
+            },
+            len: {
+                args: [10, 30],
+                msg: 'O Endereço deve ter entre 10 e 30 caracteres.'
+            },
+            is: {
+                args: /^[0-9a-z]+$/i,
+                msg: 'O Endereço para Contato não deve ter caracteres especiais.'
+            }
+        }
+    }
+});
+
 module.exports = ClientModel;
